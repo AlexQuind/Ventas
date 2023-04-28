5a42519922e7bad9054097634a64c11166a5275c
Dev_Alexander <alexander.cardona@quind.io>
2023-04-28 15:47:42 -0500

    Se crea datos en el adaptador de la capa de infraestructura con datos quemados para probar.

--- APPVentas/ventas.application/DTOs/MapDTO.cs
+++ APPVentas/ventas.application/DTOs/MapDTO.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ventas.domain.model;
 
 namespace ventas.application.DTOs
 {
 	public static class  MapDTO
 	{
-		public static ProductDTO MapProducto(ProductDTO product)
+		public static ProductDTO MapProducto(Product product)
 		{
 			return new ProductDTO
 			{
--- APPVentas/ventas.domain/model/Product.cs
+++ APPVentas/ventas.domain/model/Product.cs
@@ -11,7 +11,7 @@ namespace ventas.domain.model
 
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public double Price { get; set; }
+		public decimal Price { get; set; }
 
 	}
 
