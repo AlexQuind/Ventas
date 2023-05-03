6321bc46141847d62c7af6f529dc25241fd02034
Dev_Alexander <alexander.cardona@quind.io>
2023-05-03 14:21:08 -0500

    Se implementan configuraciones de las entidades para la base de datos.

---
+++ APPVentas/ventas.infrastructure/Configs/ProductConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ventas.domain.model;
+
+namespace ventas.infrastructure.Configs
+{
+	public class ProductConfig : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder.ToTable("Product");
+			builder.HasKey(x => x.Id);
+
+			builder.Property(x => x.Name)
+				   .HasMaxLength(50)
+				   .IsRequired();
+
+			builder.Property(x => x.Price)
+				   .HasColumnType("decimal(18,2)")
+				   .IsRequired();
+
+			builder.Property(x => x.Stock)
+				   .IsRequired();
+		}
+	}
+}
--- APPVentas/ventas.infrastructure/DbContexts/ProductContext.cs
+++ APPVentas/ventas.infrastructure/DbContexts/ProductContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ventas.domain.model;
+using ventas.infrastructure.Configs;
 
 namespace ventas.infrastructure.Context
 {
@@ -15,5 +16,12 @@ namespace ventas.infrastructure.Context
 		}
 		public DbSet<Product> Product { get; set; }
 
-    }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.ApplyConfiguration(new ProductConfig());
+		}
+
+	}
 }
