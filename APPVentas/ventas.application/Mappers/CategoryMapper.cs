56ce32633f4fab7f5a11c23560776e96cfed7b01
Dev_Alexander <alexander.cardona@quind.io>
2023-05-15 12:02:58 -0500

    Se actualizan cambios...

---
+++ APPVentas/ventas.application/Mappers/CategoryMapper.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ventas.application.DTOs;
+using ventas.domain.model;
+
+namespace ventas.application.Mappers
+{
+	public static class CategoryMapper
+	{
+		public static CategoryDTO ToDTO(Category category)
+		{
+			return new CategoryDTO
+			{
+				Id = category.Id,
+				Name = category.Name,
+				Products = category.Products?.Select(p => ProductMapper.ToDTO(p)).ToList()
+			};
+		}
+
+		public static Category ToDomain(CategoryDTO categoryDTO)
+		{
+			return new Category
+			{
+				Id = categoryDTO.Id,
+				Name = categoryDTO.Name,
+				Products = categoryDTO.Products?.Select(p => ProductMapper.ToDomain(p)).ToList()
+			};
+		}
+	}
+}
---
+++ APPVentas/ventas.infrastructure/Mappers/CategoryMapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ventas.domain.model;
+using ventas.infrastructure.Entidades;
+
+namespace ventas.infrastructure.Mappers
+{
+	public static class CategoryMapper
+	{
+		public static Category ToModel(CategoryEntity categoryEntity)
+		{
+			
+			return new Category
+			{
+				Id = categoryEntity.Id,
+				Name = categoryEntity.Name,
+				Products = categoryEntity.Products?.Select(p => p.ToModel()).ToList()
+
+			};
+		}
+
+		public static CategoryEntity ToEntity(Category categoryModel)
+		{
+			return new CategoryEntity
+			{
+				Id = categoryModel.Id,
+				Name = categoryModel.Name,
+				Products = categoryModel.Products?.Select(p => ProductMapper.ToEntity(p)).ToList()			};
+		}
+	}
+}
